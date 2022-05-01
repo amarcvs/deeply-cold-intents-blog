82bf036237702d7b734e301e414231ee10624341
Marco Alberti <alberti.1794436@studenti.uniroma1.it>
2022-05-01 22:09:56 +0200

    basic about section structure

---
+++ about/index.php
@@ -0,0 +1,12 @@
+<?php include_once($_SERVER['DOCUMENT_ROOT']."/includes/elements/header.php") ?>
+
+    <section class="about" id="about">
+        <div class="title" id="">
+            <h2>About</h2>
+            <p>Born as a university project from the need to express opinions, reflect and talk about computer science, with a greater emphasis on <strong>Cybersecurity</strong>, while maintaining an engineering approach.</p>
+        </div>
+    </section>
+
+<?php include_once($_SERVER['DOCUMENT_ROOT']."/includes/elements/footer.php") ?>
+
+
